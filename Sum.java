4fdcd95ea5d92f193d215ae45867caab3191edbc
Sundharesan2001 <92210903+Sundharesan2001@users.noreply.github.com>
2021-12-15 02:21:18 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,17 @@
+package edyoda.assignment2;
+
+public class Sum {
+    public static void main(String[] args) {
+        int a=5;
+        int b=7;
+        int c=9;
+        int sum1=a+b;
+        int sum2=a+b+c;
+        int sum3=a*b;
+        int sum4=a*b*c;
+        System.out.println("Add two numbers:"+sum1);
+        System.out.println("Add three numbers:"+sum2);
+        System.out.println("Multiple two numbers:"+sum3);
+        System.out.println("Multiple three numbers:"+sum4);
+    }
+}
