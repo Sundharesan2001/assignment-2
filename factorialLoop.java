4a6077f4ee7c05b47bb088605c9f8958c67313e3
Sundharesan2001 <92210903+Sundharesan2001@users.noreply.github.com>
2021-12-15 02:44:55 +0530

    Add files via upload

---
+++ factorialLoop.java
@@ -0,0 +1,24 @@
+package edyoda.assignment2;
+
+public class factorialLoop {
+
+    static int factorial(int n)
+    {
+        if(n == 0)
+            return 1;
+        int i = n, fact = 1;
+        while (n / i != n) {
+            fact = fact * i;
+            i--;
+        }
+        return fact;
+    }
+    public static void main(String[] args)
+    {
+        int num = 5;
+        System.out.println(
+                "Factorial of " + num
+                        + " is " + factorial(5));
+    }
+}
+
